db232dad2e5908bb0c445f085d18bca99fc17ad2
Martin Tomasi <martin.tomasi@gmail.com>
2017-03-01 22:34:43 +0100

    rustfmt

--- src/errors.rs
+++ src/errors.rs
@@ -1,3 +1,1 @@
-error_chain! {
-    
-}
+error_chain!{}
--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,8 @@
 #![allow(dead_code)]
 
 extern crate json;
-#[macro_use] extern crate error_chain;
+#[macro_use]
+extern crate error_chain;
 
 mod errors;
 
@@ -27,10 +28,22 @@ pub enum JsonType {
 
 #[derive(Debug, Clone, Copy)]
 pub enum ErrorReason {
-    TypeMismatch { expected: JsonType, found: JsonType },
-    TupleLengthMismatch { schemas: usize, tuple: usize },
-    MaxLength { expected: usize, found: usize },
-    MinLength { expected: usize, found: usize },
+    TypeMismatch {
+        expected: JsonType,
+        found: JsonType,
+    },
+    TupleLengthMismatch {
+        schemas: usize,
+        tuple: usize,
+    },
+    MaxLength {
+        expected: usize,
+        found: usize,
+    },
+    MinLength {
+        expected: usize,
+        found: usize,
+    },
 }
 
 pub trait SchemaBase {
@@ -55,7 +68,7 @@ impl<'schema> Schema<'schema> {
             Array(ref s) => s.validate(value),
             Number(ref s) => s.validate(value),
             String(ref s) => s.validate(value),
-            Integer(ref s) => s.validate(value)
+            Integer(ref s) => s.validate(value),
         }
     }
 }
@@ -72,7 +85,10 @@ impl<'schema> SchemaBase for BooleanSchema<'schema> {
             Ok(())
         } else {
             Err(ValidationError {
-                reason: ErrorReason::TypeMismatch { expected: JsonType::Boolean, found: value.get_type() },
+                reason: ErrorReason::TypeMismatch {
+                    expected: JsonType::Boolean,
+                    found: value.get_type(),
+                },
                 node: value,
             })
         }
@@ -82,9 +98,7 @@ impl<'schema> SchemaBase for BooleanSchema<'schema> {
 pub struct ObjectSchema<'schema> {
     description: Option<&'schema str>,
     id: Option<&'schema str>,
-    title: Option<&'schema str>,
-
-    // TODO
+    title: Option<&'schema str>, // TODO
 }
 
 impl<'schema> SchemaBase for ObjectSchema<'schema> {
@@ -98,33 +112,43 @@ pub struct ArraySchema<'schema> {
     max_items: Option<usize>,
     unique_items: bool,
     all_items_schema: Box<Option<Schema<'schema>>>,
-    item_schemas: Option<Vec<Schema<'schema>>>,
-    // TODO
+    item_schemas: Option<Vec<Schema<'schema>>>, // TODO
 }
 
 impl<'schema> ArraySchema<'schema> {
-    fn validate_size<'json>(&self, array: &'json [JsonValue], parent: &'json JsonValue) -> ValidationResult<'json> {
+    fn validate_size<'json>(&self,
+                            array: &'json [JsonValue],
+                            parent: &'json JsonValue)
+                            -> ValidationResult<'json> {
         if let Some(min) = self.min_items {
             if array.len() < min {
                 return Err(ValidationError {
-                    reason: ErrorReason::MinLength { expected: min, found: array.len() },
+                    reason: ErrorReason::MinLength {
+                        expected: min,
+                        found: array.len(),
+                    },
                     node: parent,
-                })
+                });
             }
         }
         if let Some(max) = self.max_items {
             if array.len() > max {
                 return Err(ValidationError {
-                    reason: ErrorReason::MaxLength { expected: max, found: array.len() },
+                    reason: ErrorReason::MaxLength {
+                        expected: max,
+                        found: array.len(),
+                    },
                     node: parent,
-                })
+                });
             }
         }
 
         Ok(())
     }
 
-    fn validate_all_items_schema<'json>(&self, array: &'json [JsonValue]) -> ValidationResult<'json> {
+    fn validate_all_items_schema<'json>(&self,
+                                        array: &'json [JsonValue])
+                                        -> ValidationResult<'json> {
         if let Some(ref schema) = *self.all_items_schema {
             for value in array {
                 schema.validate(&value)?;
@@ -134,13 +158,19 @@ impl<'schema> ArraySchema<'schema> {
         Ok(())
     }
 
-    fn validate_item_schema<'json>(&self, array: &'json [JsonValue], parent: &'json JsonValue) -> ValidationResult<'json> {
+    fn validate_item_schema<'json>(&self,
+                                   array: &'json [JsonValue],
+                                   parent: &'json JsonValue)
+                                   -> ValidationResult<'json> {
         if let Some(ref schemas) = self.item_schemas {
             if schemas.len() != array.len() {
                 return Err(ValidationError {
-                    reason: ErrorReason::TupleLengthMismatch { schemas: schemas.len(), tuple: array.len() },
-                    node: parent
-                })
+                    reason: ErrorReason::TupleLengthMismatch {
+                        schemas: schemas.len(),
+                        tuple: array.len(),
+                    },
+                    node: parent,
+                });
             }
 
             for (schema, value) in schemas.iter().zip(array) {
@@ -160,11 +190,16 @@ impl<'schema> SchemaBase for ArraySchema<'schema> {
                 self.validate_size(array, value)?;
                 self.validate_all_items_schema(array)?;
                 Ok(())
-            },
-            val => Err(ValidationError {
-                reason: ErrorReason::TypeMismatch { expected: JsonType::Array, found: val.get_type() },
-                node: value,
-            })
+            }
+            val => {
+                Err(ValidationError {
+                    reason: ErrorReason::TypeMismatch {
+                        expected: JsonType::Array,
+                        found: val.get_type(),
+                    },
+                    node: value,
+                })
+            }
         }
     }
 }
@@ -215,9 +250,10 @@ impl HasJsonType for JsonValue {
             JsonValue::Boolean(_) => JsonType::Boolean,
             JsonValue::Array(_) => JsonType::Array,
             JsonValue::Null => JsonType::Null,
-            JsonValue::String(_) | JsonValue::Short(_) => JsonType::String,
+            JsonValue::String(_) |
+            JsonValue::Short(_) => JsonType::String,
             JsonValue::Object(_) => JsonType::Object,
-            JsonValue::Number(n) => JsonType::Number
+            JsonValue::Number(n) => JsonType::Number,
         }
     }
 }
@@ -225,6 +261,5 @@ impl HasJsonType for JsonValue {
 #[cfg(test)]
 mod tests {
     #[test]
-    fn it_works() {
-    }
+    fn it_works() {}
 }
