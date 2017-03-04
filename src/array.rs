dedaad6d2bc6e4d88a425f4713fff793c0521a12
Martin Tomasi <martin.tomasi@gmail.com>
2017-03-04 12:10:44 +0100

    Work on array validation

---
+++ src/array.rs
@@ -0,0 +1,252 @@
+use json::JsonValue;
+
+use super::{Schema, ValidationError, ErrorReason, SchemaBase, JsonValueExt, JsonType};
+
+#[derive(Clone, Debug)]
+pub struct ArraySchema<'schema> {
+    description: Option<&'schema str>,
+    id: Option<&'schema str>,
+    title: Option<&'schema str>,
+
+    min_items: Option<usize>,
+    max_items: Option<usize>,
+    unique_items: bool,
+
+    all_items_schema: Box<Option<Schema<'schema>>>,
+    item_schemas: Option<Vec<Schema<'schema>>>,
+
+    additional_items: bool,
+}
+
+impl<'schema> ArraySchema<'schema> {
+    fn validate_size<'json>(&self,
+                            array: &'json [JsonValue],
+                            parent: &'json JsonValue,
+                            errors: &mut Vec<ValidationError<'json>>) {
+        if let Some(min) = self.min_items {
+            if array.len() < min {
+                errors.push(ValidationError {
+                    reason: ErrorReason::MinLength {
+                        expected: min,
+                        found: array.len(),
+                    },
+                    node: parent,
+                });
+            }
+        }
+        if let Some(max) = self.max_items {
+            if array.len() > max {
+                errors.push(ValidationError {
+                    reason: ErrorReason::MaxLength {
+                        expected: max,
+                        found: array.len(),
+                    },
+                    node: parent,
+                });
+            }
+        }
+    }
+
+    fn validate_all_items_schema<'json>(&self,
+                                        array: &'json [JsonValue],
+                                        errors: &mut Vec<ValidationError<'json>>) {
+        if let Some(ref schema) = *self.all_items_schema {
+            for value in array {
+                schema.validate(&value, errors);
+            }
+        }
+    }
+
+    fn validate_item_schema<'json>(&self,
+                                   array: &'json [JsonValue],
+                                   parent: &'json JsonValue,
+                                   errors: &mut Vec<ValidationError<'json>>) {
+        if let Some(ref schemas) = self.item_schemas {
+            if schemas.len() != array.len() && !self.additional_items {
+                errors.push(ValidationError {
+                    reason: ErrorReason::TupleLengthMismatch {
+                        schemas: schemas.len(),
+                        tuple: array.len(),
+                    },
+                    node: parent,
+                });
+            }
+
+            for (schema, value) in schemas.iter().zip(array) {
+                schema.validate(value, errors);
+            }
+        }
+    }
+
+    fn validate_unique<'json>(&self,
+                              array: &'json [JsonValue],
+                              parent: &'json JsonValue,
+                              errors: &mut Vec<ValidationError<'json>>) {
+        if self.unique_items {
+            let mut unique_items = vec![];
+            for item in array {
+                for contained in &unique_items {
+                    if *contained == item {
+                        errors.push(ValidationError {
+                            node: parent,
+                            reason: ErrorReason::ArrayItemNotUnique,
+                        });
+                        return;
+                    }
+                }
+                unique_items.push(item);
+            }
+        }
+    }
+}
+
+
+impl<'schema> SchemaBase for ArraySchema<'schema> {
+    fn validate<'json>(&self, value: &'json JsonValue, errors: &mut Vec<ValidationError<'json>>) {
+        match value {
+            &JsonValue::Array(ref array) => {
+                self.validate_size(array, value, errors);
+                self.validate_all_items_schema(array, errors);
+                self.validate_unique(array, value, errors);
+            }
+            val => {
+                errors.push(ValidationError {
+                    reason: ErrorReason::TypeMismatch {
+                        expected: JsonType::Array,
+                        found: val.get_type(),
+                    },
+                    node: value,
+                })
+            }
+        }
+    }
+}
+
+#[derive(Debug)]
+pub struct ArraySchemaBuilder<'schema> {
+    description: Option<&'schema str>,
+    id: Option<&'schema str>,
+    title: Option<&'schema str>,
+
+    min_items: Option<usize>,
+    max_items: Option<usize>,
+    unique_items: bool,
+
+    all_items_schema: Box<Option<Schema<'schema>>>,
+    item_schemas: Option<Vec<Schema<'schema>>>,
+
+    additional_items: bool,
+}
+
+impl<'schema> Default for ArraySchemaBuilder<'schema> {
+    fn default() -> ArraySchemaBuilder<'schema> {
+        ArraySchemaBuilder {
+            description: None,
+            id: None,
+            title: None,
+
+            min_items: None,
+            max_items: None,
+            unique_items: false,
+
+            all_items_schema: Default::default(),
+            item_schemas: Default::default(),
+
+            additional_items: true,
+        }
+    }
+}
+
+impl<'schema> ArraySchemaBuilder<'schema> {
+    pub fn description<V: Into<&'schema str>>(mut self, value: V) -> Self {
+        self.description = Some(value.into());
+        self
+    }
+
+    pub fn id<V: Into<&'schema str>>(mut self, value: V) -> Self {
+        self.id = Some(value.into());
+        self
+    }
+
+    pub fn title<V: Into<&'schema str>>(mut self, value: V) -> Self {
+        self.title = Some(value.into());
+        self
+    }
+
+    pub fn min_items<V: Into<usize>>(mut self, value: V) -> Self {
+        self.min_items = Some(value.into());
+        self
+    }
+
+    pub fn max_items<V: Into<usize>>(mut self, value: V) -> Self {
+        self.max_items = Some(value.into());
+        self
+    }
+
+    pub fn unique_items<V: Into<bool>>(mut self, value: V) -> Self {
+        self.unique_items = value.into();
+        self
+    }
+
+    pub fn all_items_schema<V: Into<Schema<'schema>>>(mut self, value: V) -> Self {
+        self.all_items_schema = Box::new(Some(value.into()));
+        self
+    }
+
+    pub fn item_schemas<V: Into<Vec<Schema<'schema>>>>(mut self, value: V) -> Self {
+        self.item_schemas = Some(value.into());
+        self
+    }
+
+    pub fn additional_items<V: Into<bool>>(mut self, value: V) -> Self {
+        self.additional_items = value.into();
+        self
+    }
+
+    pub fn build(self) -> ArraySchema<'schema> {
+        ArraySchema {
+            description: self.description,
+            id: self.id,
+            title: self.title,
+
+            min_items: self.min_items,
+            max_items: self.max_items,
+            unique_items: self.unique_items,
+
+            all_items_schema: self.all_items_schema,
+            item_schemas: self.item_schemas,
+
+            additional_items: self.additional_items,
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use json;
+    use super::*;
+
+    #[test]
+    fn unique_elements() {
+        let schema = ArraySchemaBuilder::default()
+            .unique_items(true)
+            .build();
+        let json = json::parse("[1, 1, 2, 3, 4]").unwrap();
+        let mut errors = vec![];
+        schema.validate(&json, &mut errors);
+        println!("{:?}", errors);
+        assert_eq!(errors.len(), 1);
+        assert_eq!(errors[0].reason, ErrorReason::ArrayItemNotUnique);
+    }
+
+    #[test]
+    fn default_schema() {
+        let schema = ArraySchemaBuilder::default().build();
+        let input = json::parse(r#"[1, "a", "b", {"test": 123}, []]"#).unwrap();
+        let mut errors = vec![];
+        schema.validate(&input, &mut errors);
+        println!("{:?}", errors);
+        assert_eq!(errors.len(), 0)
+        
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -7,20 +7,24 @@ extern crate json;
 extern crate error_chain;
 
 mod errors;
+mod array;
 
-use std::collections::{HashMap, HashSet};
+use std::collections::HashMap;
 
 use json::JsonValue;
 use json::object::Object;
 
+use array::ArraySchema;
+
 pub type ValidationResult<'json> = Result<(), ValidationError<'json>>;
 
+#[derive(Debug, Clone)]
 pub struct ValidationError<'json> {
     reason: ErrorReason,
     node: &'json JsonValue,
 }
 
-#[derive(Debug, Clone, Copy)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub enum JsonType {
     Null,
     Boolean,
@@ -31,32 +35,21 @@ pub enum JsonType {
     Integer,
 }
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, PartialEq, Eq)]
 pub enum ErrorReason {
-    TypeMismatch {
-        expected: JsonType,
-        found: JsonType,
-    },
-    TupleLengthMismatch {
-        schemas: usize,
-        tuple: usize,
-    },
-    MaxLength {
-        expected: usize,
-        found: usize,
-    },
-    MinLength {
-        expected: usize,
-        found: usize,
-    },
+    TypeMismatch { expected: JsonType, found: JsonType },
+    TupleLengthMismatch { schemas: usize, tuple: usize },
+    MaxLength { expected: usize, found: usize },
+    MinLength { expected: usize, found: usize },
     MissingProperty(String),
-    ArrayItemNotUnique {}
+    ArrayItemNotUnique,
 }
 
 pub trait SchemaBase {
     fn validate<'json>(&self, value: &'json JsonValue, errors: &mut Vec<ValidationError<'json>>);
 }
 
+#[derive(Clone, Debug)]
 pub enum Schema<'schema> {
     Boolean(BooleanSchema<'schema>),
     Object(ObjectSchema<'schema>),
@@ -80,6 +73,7 @@ impl<'schema> SchemaBase for Schema<'schema> {
     }
 }
 
+#[derive(Clone, Debug)]
 pub struct BooleanSchema<'schema> {
     description: Option<&'schema str>,
     id: Option<&'schema str>,
@@ -100,6 +94,7 @@ impl<'schema> SchemaBase for BooleanSchema<'schema> {
     }
 }
 
+#[derive(Clone, Debug)]
 pub struct ObjectSchema<'schema> {
     description: Option<&'schema str>,
     id: Option<&'schema str>,
@@ -148,124 +143,7 @@ impl<'schema> SchemaBase for ObjectSchema<'schema> {
     }
 }
 
-pub struct ArraySchema<'schema> {
-    description: Option<&'schema str>,
-    id: Option<&'schema str>,
-    title: Option<&'schema str>,
-
-    min_items: Option<usize>,
-    max_items: Option<usize>,
-    unique_items: bool,
-    all_items_schema: Box<Option<Schema<'schema>>>,
-    item_schemas: Option<Vec<Schema<'schema>>>,
-}
-
-impl<'schema> ArraySchema<'schema> {
-    fn validate_size<'json>(&self,
-                            array: &'json [JsonValue],
-                            parent: &'json JsonValue,
-                            errors: &mut Vec<ValidationError<'json>>) {
-        if let Some(min) = self.min_items {
-            if array.len() < min {
-                errors.push(ValidationError {
-                    reason: ErrorReason::MinLength {
-                        expected: min,
-                        found: array.len(),
-                    },
-                    node: parent,
-                });
-            }
-        }
-        if let Some(max) = self.max_items {
-            if array.len() > max {
-                errors.push(ValidationError {
-                    reason: ErrorReason::MaxLength {
-                        expected: max,
-                        found: array.len(),
-                    },
-                    node: parent,
-                });
-            }
-        }
-    }
-
-    fn validate_all_items_schema<'json>(&self,
-                                        array: &'json [JsonValue],
-                                        errors: &mut Vec<ValidationError<'json>>) {
-        if let Some(ref schema) = *self.all_items_schema {
-            for value in array {
-                schema.validate(&value, errors);
-            }
-        }
-    }
-
-    fn validate_item_schema<'json>(&self,
-                                   array: &'json [JsonValue],
-                                   parent: &'json JsonValue,
-                                   errors: &mut Vec<ValidationError<'json>>) {
-        if let Some(ref schemas) = self.item_schemas {
-            if schemas.len() != array.len() {
-                errors.push(ValidationError {
-                    reason: ErrorReason::TupleLengthMismatch {
-                        schemas: schemas.len(),
-                        tuple: array.len(),
-                    },
-                    node: parent,
-                });
-            }
-
-            for (schema, value) in schemas.iter().zip(array) {
-                schema.validate(value, errors);
-            }
-        }
-    }
-
-    fn validate_unique<'json>(&self,
-                              array: &'json [JsonValue],
-                              parent: &'json JsonValue,
-                              errors: &mut Vec<ValidationError<'json>>) {
-        // TODO implement PartialEq<JsonValue> for JsonValue
-        /*
-        if self.unique_items {
-            let mut unique_items = vec![];
-            for item in array {
-                for contained in &unique_items {
-                    if contained == item {
-                        errors.push(ValidationError {
-                            node: parent,
-                            reason: ErrorReason::ArrayItemNotUnique {}
-                        });
-                        return;
-                    }
-                }
-                unique_items.push(item);
-            }
-        }*/
-    }
-}
-
-
-impl<'schema> SchemaBase for ArraySchema<'schema> {
-    fn validate<'json>(&self, value: &'json JsonValue, errors: &mut Vec<ValidationError<'json>>) {
-        match value {
-            &JsonValue::Array(ref array) => {
-                self.validate_size(array, value, errors);
-                self.validate_all_items_schema(array, errors);
-                self.validate_unique(array, value, errors);
-            }
-            val => {
-                errors.push(ValidationError {
-                    reason: ErrorReason::TypeMismatch {
-                        expected: JsonType::Array,
-                        found: val.get_type(),
-                    },
-                    node: value,
-                })
-            }
-        }
-    }
-}
-
+#[derive(Clone, Debug)]
 pub struct NumberSchema<'schema> {
     description: Option<&'schema str>,
     id: Option<&'schema str>,
@@ -278,6 +156,7 @@ impl<'schema> SchemaBase for NumberSchema<'schema> {
     }
 }
 
+#[derive(Clone, Debug)]
 pub struct StringSchema<'schema> {
     description: Option<&'schema str>,
     id: Option<&'schema str>,
@@ -290,6 +169,7 @@ impl<'schema> SchemaBase for StringSchema<'schema> {
     }
 }
 
+#[derive(Clone, Debug)]
 pub struct IntegerSchema<'schema> {
     description: Option<&'schema str>,
     id: Option<&'schema str>,
@@ -315,36 +195,14 @@ impl JsonValueExt for JsonValue {
             JsonValue::String(_) |
             JsonValue::Short(_) => JsonType::String,
             JsonValue::Object(_) => JsonType::Object,
-            JsonValue::Number(_) => JsonType::Number,
+            JsonValue::Number(_) => {
+                let n = self.as_f64().unwrap();
+                if n.trunc() == n {
+                    JsonType::Integer
+                } else {
+                    JsonType::Number
+                }
+            }
         }
-    }
-}
-
-pub struct SchemaBuilder<'s> {
-    root: Schema<'s>
-}
-
-#[cfg(test)]
-mod tests {
-    use json;
-    use super::*;
-
-    #[test]
-    fn array_schema() {
-        let input = "[1, 2, 3, 4]";
-        let schema = ArraySchema {
-            description: None,
-            id: None,
-            title: None,
-            min_items: Some(1),
-            max_items: Some(2),
-            unique_items: false,
-            all_items_schema: Box::new(None),
-            item_schemas: None,
-        };
-        let json = json::parse(input).unwrap();
-        let mut errors = vec![];
-        schema.validate(&json, &mut errors);
-        assert!(errors.len() > 0);
     }
 }
